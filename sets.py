9e7e28c1e31eabf974c618034ddae62d0f4f85e0
Anas Zaheer <m.anas.zaheer@gmail.com>
2026-07-07 01:25:00 +0500

    final upload of july 7 at 1:24pm

--- sets.py
+++ sets.py
@@ -23,7 +23,50 @@
 # print(thisset)
 
 #ADD SETS
-thisset = {"apple", "banana", "cherry"}
-tropical = {"pineapple", "mango", "papaya"}
-thisset.update(tropical)
-print(thisset)
+# thisset = {"apple", "banana", "cherry"}
+# tropical = {"pineapple", "mango", "papaya"}
+# thisset.update(tropical)
+# print(thisset)
+
+#REMOVE ITEMS
+# thisset = {"apple", "banana", "cherry"}
+# thisset.remove("banana")
+# print(thisset)
+
+#POP ITEM (REMOVE)
+# thisset = {"apple", "banana", "cherry"}
+# x = thisset.pop()
+# print(x)
+# print(thisset)
+
+#LOOP ITEMS
+# thisset = {"apple", "banana", "cherry"}
+# for x in thisset:
+#   print(x)
+
+#JOIN SETS
+#Union - returns a new set with all items from both sets
+# set1 = {"a", "b", "c"}
+# set2 = {1, 2, 3}
+# set3 = set1.union(set2)
+# print(set3)
+#Update - inserts all items from one set into another (does not create new set)
+# set1 = {"a", "b" , "c"}
+# set2 = {1, 2, 3}
+# set1.update(set2)
+# print(set1)
+#Intersection - returns a new set, that only contains the items that are present in both sets
+# set1 = {"apple", "banana", "cherry"}
+# set2 = {"google", "microsoft", "apple"}
+# set3 = set1.intersection(set2)
+# print(set3)
+#Difference - returns a new set, containing only items from first set not present in other set
+# set1 = {"apple", "banana", "cherry"}
+# set2 = {"google", "microsoft", "apple"}
+# set3 = set1.difference(set2)
+# print(set3)
+#Symmetric Difference - keeps only the elements that are NOT present in both sets
+# set1 = {"apple", "banana", "cherry"}
+# set2 = {"google", "microsoft", "apple"}
+# set3 = set1.symmetric_difference(set2)
+# print(set3)
