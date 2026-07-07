2bedf857123e3524b60e72e6151b53f7874480b9
Anas Zaheer <m.anas.zaheer@gmail.com>
2026-07-08 00:03:26 +0500

    final for 8 july

--- dictionaries.py
+++ dictionaries.py
@@ -75,6 +75,15 @@
 # for x in thisdict.keys():
 #     print(x)
 
+# COPY DICTIONARY
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# mydict = thisdict.copy()
+# print(mydict)
+
 #IF EXISTS
 # thisdict = {
 #   "brand": "Ford",
@@ -83,3 +92,27 @@
 # }
 # if "model" in thisdict:
 #   print("Yes, 'model' is one of the keys in the thisdict dictionary")
+
+#NESTED DICTIONARIES
+# child1 = {
+#   "name" : "Emil",
+#   "year" : 2004
+# }
+# child2 = {
+#   "name" : "Tobias",
+#   "year" : 2007
+# }
+# child3 = {
+#   "name" : "Linus",
+#   "year" : 2011
+# }
+# myfamily = {
+#   "child1" : child1,
+#   "child2" : child2,
+#   "child3" : child3
+# }
+# print(myfamily["child2"]["name"])
+# for x, obj in myfamily.items():
+#     print(x)
+#     for y in obj:
+#         print(y + ":", obj[y])
